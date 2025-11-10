da6edba0370f49db92d6fce2ab99785d4492c864
Taylor <taylorpei@futurefab.ai>
2025-11-10 14:56:21 +0800

    project init

---
+++ src/main/java/com/example/function_demo/FunctionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.function_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionDemoApplication.class, args);
+	}
+
+}
